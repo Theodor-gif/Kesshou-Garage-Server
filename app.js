934b1eee2ff8e3a22878404303e5c77b8c86bf70
Theodoros Mitropoulos <theodor.mitropoulos@yahoo.com>
2026-08-19 19:00:05 +0200

    Add dynamic CORS origin and npm start script

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static("public"));
 app.use(morgan("dev"));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true,
   }),
 );
