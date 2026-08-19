302ba86f74fe21461cd8a53b8323113f7c2cd11d
Theodoros Mitropoulos <theodor.mitropoulos@yahoo.com>
2026-08-19 19:55:54 +0200

    Change

--- app.js
+++ app.js
@@ -21,9 +21,23 @@ const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(express.static("public"));
 app.use(morgan("dev"));
+const allowedOrigins = [
+  "http://localhost:5173", // or your local port 3000/5000
+  "https://kesshou-garage-client-five.vercel.app",
+];
+
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
     credentials: true,
   }),
 );
